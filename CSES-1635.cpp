01292f9ced1b5bbd2e72e8110ffdaadf491cca2f
Theodora91131 <10556003@ntub.edu.tw>
2025-01-26 20:42:21 +0800

    Algorithm

---
+++ CSES-1635.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+const int MOD = 1e9 + 7;
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int> coin(n);
+    vector<int> dp(x, 0);
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> coin[i];
+    }
+    dp[0] = 1;
+
+    for (int i = 1; i <= x; ++i)
+    {
+        long long sum = 0;
+        for (int j = 1; j <= n; ++j)
+        {
+            if (i - coin[j] >= 0)
+            {
+                sum += dp[i - coin[j]];
+            }
+        }
+        dp[i] = sum % MOD;
+    }
+    // cout << dp[x] << endl;
+    return 0;
+}
