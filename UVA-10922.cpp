6733b0f28305f74dedaaac6e6b7ee99130083935
Theodora91131 <10556003@ntub.edu.tw>
2025-02-07 00:17:43 +0800

    Algorithm

--- UVA-10922.cpp
+++ UVA-10922.cpp
@@ -1,4 +1,35 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+// 計算數位和，並遞迴計算 9-degree
+int nine_degree(string num, int depth = 1) {
+    int sum = 0;
+    for (char c : num) {
+        sum += c - '0'; //stoi只能用在string str[i]是char所以不能這樣做 這題類似UVA-10252
+    }
+
+    if (sum == 9) return depth;
+    if (sum < 9) return 0;
+    return nine_degree(to_string(sum), depth + 1); // 遞迴計算
+}
+
+int main() {
+    string num;
+    while (cin >> num && num != "0") {
+        int degree = nine_degree(num);
+
+        if (degree == 0) {
+            cout << num << " is not a multiple of 9." << endl;
+        }
+        else {
+            cout << num << " is a multiple of 9 and has 9-degree " << degree << "." << endl;
+        }
+    }
+    return 0;
+}
+//////////////////法二///////////////////////////
+/**#include<iostream>
 #include<vector>
 #include<string>
 #include<algorithm>
@@ -28,4 +59,4 @@ int main() {
         }
     }
     return 0;
-}
+}**/
